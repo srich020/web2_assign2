57fd63feb07a52cd9d696559469c58241156d685
Stepw868 <cruessa@gmail.com>
2016-11-17 16:39:54 -0700

    About Us Updated

--- aboutus.php
+++ aboutus.php
@@ -18,9 +18,9 @@ include './inc/header.inc.php';?>
     Project Information
   </div>
   <ul class="list">
-    <li>Authors: Sebastian Richters, Andrew Cruess, David Han</li>
+    <li>Mount Royal University: BCIS Program</li>
     <li>COMP3512 Web Development II - Fall 2016</li>
-	<li>November 19th, 2016</li>
+	<li>Delivered on November 19th, 2016</li>
 	</ul>
 	<div class="ui hidden divider"></div>
 	<div class="header">
@@ -31,6 +31,40 @@ include './inc/header.inc.php';?>
 	<li>JavaScript</li>
 	<li>PHP Sessions</li>
 	</ul>
+	<table class="ui celled striped table">
+  <thead>
+    <tr><th colspan="2">
+      Team Members and Responsibilities
+    </th>
+  </tr></thead>
+  <tbody>
+    <tr>
+      <td class="collapsing">
+        <i class="user icon"></i> Andrew Cruess
+      </td>
+      <td><ul><li>Search Functionality</li><li>Browse Galleries</li><li>Single Gallery</li><li>Update Old Pages</li><li>Google Maps Integration</li><li>Testing & Refinement for Delivery</li>
+	  </ul></td>
+      
+    </tr>
+    <tr>
+      <td class="collapsing">
+        <i class="user icon"></i> Sebastian Richters
+      </td>
+      <td><ul><li>Data Access Layer</li><li>Cart Integration & Functionality</li><li>Single Subject</li><li>Browse Subjects</li><li>Retrofitting data access layer into old pages</li>
+	  </ul></td>
+      
+    </tr>
+    <tr>
+      <td class="collapsing">
+        <i class="user icon"></i> David Han
+      </td>
+      <td><ul><li>Session State Functionality</li><li>Favorites Integration & Functionality</li><li>Cart Integration</li><li>Fixing Semantic UI Issues</li>
+	  </ul></td>
+      
+    </tr>
+    
+  </tbody>
+</table>
 </div>
 </div>
 </div>
